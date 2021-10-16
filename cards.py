5c51672d45c76eba0889f02034d9b1047a1dc6d0
OwOverlord <lucaslej19@gmail.com>
2021-10-16 23:36:52 +0200

    Adding Comments.

--- cards.py
+++ cards.py
@@ -1,9 +1,12 @@
 from collections import namedtuple
 
-Card = namedtuple('Card', 'color number')
-deck = [Card(color=col, number=num) for col in ["hearts", "diamonds", "spades", "clubs"]
+# HACK: turned sera modifiable en utilisant la méthode ._replace()
+Card = namedtuple('Card', 'color number turned')
+
+deck = [Card(color=col, number=num, turned=False) for col in ["hearts", "diamonds", "spades", "clubs"]
         for num in range(1, 14)]
 
-jokers = [Card(color="joker", number=14) for _ in range(2)]
+jokers = [Card(color="joker", number=14, turned=False) for _ in range(2)]
 
+# TADA! Le jeu de cartes complet est déjà créé en à peine quelques lignes.
 FULL_DECK = deck + jokers
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from cards import FULL_DECK
+from cards import FULL_DECK, Cards
 from copy import deepcopy
 import random
 from collections import deque
+from typing import List
+
 
 c = deque(deepcopy(FULL_DECK))
 random.shuffle(c)
-playing_board = []
+
+
+playing_board: List[List[Cards]] = []
 
 for a in range(7):
     playing_board.append([c.pop() for _ in range(a+1)])
 
-print(playing_board)
+    # print(playing_board)
