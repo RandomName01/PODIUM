9d33c36a036aaa0474ee67b5e84c28f33942bda8
OwOverlord <lucaslej19@gmail.com>
2021-10-17 13:49:44 +0200

    Adding some logic to the game.

--- logic.py
+++ logic.py
@@ -1,13 +1,17 @@
 from cards import Cards
 
+
 def black_or_red(card: Cards) -> str:
     return "black" if card.color in ("clubs", "spades") else "red"
 
 
-
 def can_follow(card_1: Cards, card_2: Cards) -> bool:
     """
-    Checks if a card can follow another.
+    Checks if a card can follow another on the stack.
+    A card can follow another card iff:
+    -> The colors of the two cards are different.
+    -> The number on the card = the number of the card that's being followed - 1.
+    -> A joker can actually follow any other card.
     """
     return black_or_red(card_1) != black_or_red(card_2) & \
         card_1.number - 1 == card_2.number | \
