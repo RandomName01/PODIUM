986fd0b25cc099f224419c09e488799b22bf7aab
OwOverlord <lucaslej19@gmail.com>
2021-10-17 15:19:37 +0200

    Adding the brand new flip_card function.

--- cards.py
+++ cards.py
@@ -1,13 +1,17 @@
 from collections import namedtuple
 
-# HACK: turned sera modifiable en utilisant la méthode ._replace()
+# HACK: turned sera modifiable en utilisant la fonction flip_card
 Card = namedtuple('Card', 'color number flipped')
 
-deck = [Card(color=col, number=num, flipped=False)
+DECK = [Card(color=col, number=num, flipped=False)
         for col in ["hearts", "diamonds", "spades", "clubs"]
         for num in range(1, 14)]
 
-jokers = [Card(color="joker", number=14, flipped=False) for _ in range(2)]
+JOKERS = [Card(color="joker", number=14, flipped=False) for _ in range(2)]
 
 # TADA! Le jeu de cartes complet est déjà créé en à peine quelques lignes. et oui!
-FULL_DECK = deck + jokers
+FULL_DECK = DECK + JOKERS
+
+
+def flip_card(card: Card) -> Card:
+    return card._replace(flipped=not card.flipped)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from cards import FULL_DECK, Card
+from cards import FULL_DECK, Card, flip_card
 from copy import deepcopy
 import random
 from collections import deque
@@ -13,6 +13,6 @@ playing_board: List[List[Card]] = []
 
 for a in range(7):
     stack = [c.pop() for _ in range(a+1)]
-    playing_board.append([stack[0]._replace(flipped=True)] + stack[1:])
+    playing_board.append([flip_card(stack[0])] + stack[1:])
 
     # print(playing_board)
